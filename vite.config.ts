83df2c0086c808c70043a7153f13b53bf20ccd8e
Александр Троицкий <alexander.troitsky@gmail.com>
2025-11-09 20:20:55 +0300

    Изменена структура папок для публикации в github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         vue(),
         vueDevTools()
     ],
-    base: "/calendar-app/",
+    base: "/calendar-demo-app/",
     define: {
         '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
     },
