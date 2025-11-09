2dd27a9b41082f16d777776976c742ef73d13e92
Александр Троицкий <alexander.troitsky@gmail.com>
2025-11-10 00:03:27 +0300

    Исправления для корректной публикации в github pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <HeaderView :title="$t('titles.header_title')" />
-  <RouterView />
+  <ContentView />
   <FooterView :title="$t('titles.footer_title')"/>
 </template>
 
 <script setup lang="ts">
 import HeaderView from './components/HeaderView.vue'
+import ContentView from '@/components/ContentView.vue'
 import FooterView from './components/FooterView.vue'
 
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import { createI18n } from 'vue-i18n'
 import './style.css'
 import App from './App.vue'
-import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css';
 import ru from 'element-plus/es/locale/lang/ru';
@@ -17,7 +16,6 @@ const i18n = createI18n({
 })
 const app = createApp(App)
     .use(createPinia())
-    .use(router)
     .use(i18n)
     .use(ElementPlus)
     .mount('#app')
